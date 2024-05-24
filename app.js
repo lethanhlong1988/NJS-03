4957f277993bc1794eb683a37cd77b4c4e2d3498
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-24 04:40:27 +0000

    update

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log("Preparing...!!!");
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+  }
+  if (url === "/message" && method === "POST") {
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!!!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 server.listen(3000);
