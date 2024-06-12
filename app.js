e5ddca268eff247ef75a0c24e82a82515a70a3ef
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-12 07:36:38 +0000

    update

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log(req.headers, req.url, req.method);
+  //   process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head>");
+
+  res.write("<head>");
+  res.write("<head><head>");
+
+  res.write("<body></body>");
+  res.write("</html>");
+});
 server.listen(3000);
