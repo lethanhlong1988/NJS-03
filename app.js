7edcf0d16ebba6cb3e3a6fbbe30d66bce6df5186
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-13 06:12:12 +0000

    update

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  //   process.exit();
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='meddage'/><button type='submit'>Send</button></form></body>",
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-    });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h3>Hello from my Node.js Server!!!</h3></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 server.listen(3000);
