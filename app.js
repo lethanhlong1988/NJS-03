780a9d468edbb31d8e038ec8428f79dffecd5231
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-06-13 02:11:23 +0000

    update

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers, req.url, req.method);
   //   process.exit();
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='meddage'/><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head>");
-
-  res.write("<head>");
   res.write("<head><title>My First Page</title><head>");
-
   res.write("<body><h3>Hello from my Node.js Server!!!</h3></body>");
   res.write("</html>");
   res.end();
